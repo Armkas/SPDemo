34c377841f1a615fb9f9d13a89f719d98aa54408
Arakasu <puyue2023@gmail.com>
2022-10-30 17:41:26 +0900

    写UI, 关联UI和代码

--- app/src/main/java/com/example/spdemo/MainActivity.kt
+++ app/src/main/java/com/example/spdemo/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.spdemo
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  nameText: EditText
+    lateinit var ageText: EditText// 初始化时候可以不赋值 类似于 ios ?
+    lateinit var sf: SharedPreferences//这是一个存储用的东西 全app内共通 存储和写入数据  类似于UnserDefault key-value形式保存
+    lateinit var editor: SharedPreferences.Editor//
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameText = findViewById(R.id.etName)//通过ID关联UI和代码
+        ageText = findViewById(R.id.etAge)
+
+        sf = getSharedPreferences("my_sf", MODE_PRIVATE)//存储的key是my_sf
+        editor = sf.edit()
+    }
+
+    override fun onPause() {//app到后台前 存储数据到SharedPreferences
+        super.onPause()
+        val name = nameText.text.toString()//处理用户输入字符
+        val age = ageText.text.toString().toInt()//处理用户输入字符
+        editor.apply {
+            putString("sf_name", name)
+            putInt("sf_age", age)
+            commit()//??????
+        }
+    }
+
+    override fun onResume() {//app恢复台前 从SharedPreferences读取数据
+        super.onResume()
+        val name = sf.getString("sf_name", null)
+        val age = sf.getInt("sf_age", 0)
+        nameText.setText(name)
+        if (age != 0) {
+            ageText.setText(age.toString())
+        }
     }
 }
